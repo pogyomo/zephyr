1070ee5685eea4f14f8fc22be7d0c2fdbb9c2b56
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2023-07-06 11:48:12 +0900

    feat: add function call expression

--- zephyr-ast/src/expr.rs
+++ zephyr-ast/src/expr.rs
@@ -17,6 +17,7 @@ impl_from!(Expression, IdentExpr, IntExpr, UnaryExpr, InfixExpr);
 pub enum Expression {
     IdentExpr(IdentExpr),
     IntExpr(IntExpr),
+    FuncCallExpr(FuncCallExpr),
     UnaryExpr(UnaryExpr),
     InfixExpr(InfixExpr),
 }
@@ -26,6 +27,7 @@ impl Spannable for Expression {
         match self {
             Expression::IdentExpr(ident) => ident.span(),
             Expression::IntExpr(int) => int.span(),
+            Expression::FuncCallExpr(call) => call.span(),
             Expression::UnaryExpr(unary) => unary.span(),
             Expression::InfixExpr(infix) => infix.span(),
         }
@@ -46,13 +48,40 @@ impl Spannable for IdentExpr {
 }
 
 #[derive(new)]
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct IntExpr {
     span: Span,
     pub value: u64,
 }
 
 impl Spannable for IntExpr {
+    fn span(&self) -> Span {
+        self.span
+    }
+}
+
+#[derive(new)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct FuncCallExpr {
+    span: Span,
+    pub name: FuncCallExprName,
+    pub args: Vec<Expression>,
+}
+
+impl Spannable for FuncCallExpr {
+    fn span(&self) -> Span {
+        self.span
+    }
+}
+
+#[derive(new)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct FuncCallExprName {
+    span: Span,
+    pub name: String,
+}
+
+impl Spannable for FuncCallExprName {
     fn span(&self) -> Span {
         self.span
     }
